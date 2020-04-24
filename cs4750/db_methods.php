3b2d80d59449a44dc946c897ea6407fb7a530349
Shahroz Imtiaz <shahrozimtiaz07@gmail.com>
2020-04-24 17:15:07 -0400

    added reviews

--- cs4750/db_methods.php
+++ cs4750/db_methods.php
@@ -368,5 +368,77 @@ function getCrimeHistory() {
   $statement->closecursor();
   return $results;
 }
+function getReviews(){
+   global $db;
+   $query = "select * from review order by Date desc";
+   $statement = $db->prepare($query);
+   $statement->execute();
+   $results = $statement->fetchAll();
+   $statement->closecursor();
+   return $results;
+}
 
+function createReview($username,$title,$text,$rating){
+   global $db;
+   $query = "insert into review(User_Name,Rating,Title,Text) values (:username,:rating,:title,:text)";
+   $statement = $db->prepare($query);
+   $statement->bindValue(':username', $username);
+   $statement->bindValue(':title', $title);
+   $statement->bindValue(':text', $text);
+   $statement->bindValue(':rating', $rating);
+   if ($statement->execute()){
+      $results = TRUE;
+   }else{
+      $results = FALSE;
+   }
+   $statement->closecursor();
+   return $results;
+}
+
+function getReview($id){
+   global $db;
+   $query = "select * from review where Review_ID=:id";
+   $statement = $db->prepare($query);
+   $statement->bindValue(':id', $id);
+   if ($statement->execute()){
+      $results = $statement->fetch();
+   }else{
+      $results = FALSE;
+   }
+   $statement->closecursor();
+   return $results;
+}
+
+function updateReview($id,$username,$title,$text,$rating){
+   global $db;
+   echo $id;
+   $query = "update review set User_Name=:username, Rating=:rating, Title=:title, Text=:text where Review_ID=:id";
+   $statement = $db->prepare($query);
+   $statement->bindValue(':id', $id);
+   $statement->bindValue(':username', $username);
+   $statement->bindValue(':title', $title);
+   $statement->bindValue(':text', $text);
+   $statement->bindValue(':rating', $rating);
+   if ($statement->execute()){
+      $results = TRUE;
+   }else{
+      $results = FALSE;
+   }
+   $statement->closecursor();
+   return $results;
+}
+
+function deleteReview($id){
+   global $db;
+   $query = "delete from review where Review_ID=:id";
+   $statement = $db->prepare($query);
+   $statement->bindValue(':id', $id);
+   if ($statement->execute()){
+      $results = TRUE;
+   }else{
+      $results = FALSE;
+   }
+   $statement->closecursor();
+   return $results;
+}
 ?>
