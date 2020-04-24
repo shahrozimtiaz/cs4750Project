6bdf2d1dcf1d24a4e2be935acd32106ae9814c69
Ryan Riley <45950573+rr2nz@users.noreply.github.com>
2020-04-24 16:20:01 -0400

    Add files via upload

---
+++ db_methods.php
@@ -0,0 +1,356 @@
+<!-- functions for querying db -->
+<?php
+function login($username,$password){
+   global $db;
+   $query = "select Password from user where User_Name=:username";
+   $statement = $db->prepare($query);
+   $statement->bindValue(':username', $username);
+   $statement->execute();
+   $results = $statement->fetch();
+   $statement->closecursor();
+   if(!$results){
+      return FALSE;
+   }
+   return password_verify($password,$results['Password']);
+}
+
+function signup($username,$password){
+   global $db;
+   $query = "insert into user(User_Name, Password) values (:username,:password)";
+   $statement = $db->prepare($query);
+   $statement->bindValue(':username', $username);
+   $statement->bindValue(':password', password_hash($password,PASSWORD_DEFAULT));
+   if ($statement->execute()){
+      $results = TRUE;
+   }else{
+      $results = FALSE;
+   }
+   $statement->closecursor();
+   return $results;
+}
+
+function getAirlines(){
+   global $db;
+   $query = "select * from airline";
+   if (isset($_POST['airlineName']) && $_POST['airlineName']!='any') {
+      $params[] = 'Name=:name';
+   }
+   if (isset($_POST['airlineIncidents']) && $_POST['airlineIncidents']!='any') {
+       if (strpos($_POST['airlineIncidents'], '>') !== false) {
+           $params[] = 'Incidents > :incidents';
+       } else if (strpos($_POST['airlineIncidents'], '<') !==false) {
+           $params[] = 'Incidents < :incidents';
+       } else {
+           $params[] = 'Incidents=:incidents';
+       }
+   }
+   if (isset($_POST['airlineFatalAccidents']) && $_POST['airlineFatalAccidents']!='any') {
+       if (strpos($_POST['airlineFatalAccidents'], '>') !== false) {
+           $params[] = 'Fatal_Accidents > :fatalAccidents';
+       } else if (strpos($_POST['airlineFatalAccidents'], '<') !==false) {
+           $params[] = 'Fatal_Accidents < :fatalAccidents';
+       } else {
+           $params[] = 'Fatal_Accidents=:fatalAccidents';
+       }
+   }
+   if (isset($_POST['airlineFatalities']) && $_POST['airlineFatalities']!='any') {
+       if (strpos($_POST['airlineFatalities'], '>') !== false) {
+           $params[] = 'Fatalities > :fatalities';
+       } else if (strpos($_POST['airlineFatalities'], '<') !==false) {
+           $params[] = 'Fatalities < :fatalities';
+       } else {
+           $params[] = 'Fatalities=:fatalities';
+       }
+   }
+   if (!empty($params)) {
+      $query .= ' where ' . implode(' AND ', $params);
+   }
+   $statement = $db->prepare($query);
+   if (isset($_POST['airlineName']) && $_POST['airlineName']!='any') {
+      $statement->bindValue(':name', $_POST['airlineName']);
+   }
+   if (isset($_POST['airlineIncidents']) && $_POST['airlineIncidents']!='any') {
+       $statement->bindValue('incidents', trim($_POST['airlineIncidents'], '<> '));
+   }
+   if (isset($_POST['airlineFatalAccidents']) && $_POST['airlineFatalAccidents']!='any') {
+      $statement->bindValue(':fatalAccidents', trim($_POST['airlineFatalAccidents'], "<> "));
+   }
+   if (isset($_POST['airlineFatalities']) && $_POST['airlineFatalities']!='any') {
+      $statement->bindValue(':fatalities', trim($_POST['airlineFatalities'], '<> '));
+   }
+
+   $statement->execute();
+   $results = $statement->fetchAll();
+   $statement->closecursor();
+
+   if ($query != "select * from airline") {
+     // Inserting into query_history table
+     $user_id = $_SESSION['loggedin'];
+     $query1 = "INSERT INTO `query_history` (`User_ID`, `Query_ID`, `Date_Time`) VALUES ('$user_id', NULL, current_timestamp())";
+     $statement = $db->prepare($query1);
+     $statement->execute();
+
+     // Inserting into query_history_airline
+     foreach ($results as $result) {
+       $name = $result['Name'];
+       $query = "INSERT INTO `query_history_airline` (`User_ID`, `Query_ID`, `Date_Time`, `Name`)
+       VALUES ('$user_id', NULL, current_timestamp(), '$name')";
+       $statement = $db->prepare($query);
+       $statement->execute();
+     }
+   }
+
+   return $results;
+}
+
+function getAirbnb(){
+   global $db;
+   $query = "select * from airbnbhost ";
+   if (isset($_POST['airbnbListingName']) && $_POST['airbnbListingName']!='any') {
+      $query.= "JOIN airbnblist ON airbnbhost.Host_Id = airbnblist.Host_Id";
+      $params[] = 'Listing_ID=:listing_id';
+   }
+   if (isset($_POST['airbnbLocation']) && $_POST['airbnbLocation']!='any') {
+      if(strpos($query,'JOIN airbnblist ON airbnbhost.Host_Id') == false){
+         $query.= "JOIN airbnblist ON airbnbhost.Host_Id = airbnblist.Host_Id";
+      }
+      $params[] = "Location LIKE CONCAT( '%', :location, '%' )";
+   }
+   if (isset($_POST['airbnbAmenities']) && $_POST['airbnbAmenities']!='any') {
+      if(strpos($query,'JOIN airbnblist ON airbnbhost.Host_Id') == false){
+         $query.= "JOIN airbnblist ON airbnbhost.Host_Id = airbnblist.Host_Id JOIN airbnblisting_amenities ON airbnblist.Listing_ID = airbnblisting_amenities.Listing_ID ";
+         $params[] = "Amenity LIKE CONCAT( '%', :amenity, '%' )";
+      }else{
+         $query.=" JOIN airbnblisting_amenities ON airbnblist.Listing_ID = airbnblisting_amenities.Listing_ID ";
+         $params[] = "Amenity LIKE CONCAT( '%', :amenity, '%' )";
+      }
+   }
+   if (isset($_POST['airbnbRating']) && $_POST['airbnbRating']!='any') {
+      if(strpos($query,'JOIN airbnblist ON airbnbhost.Host_Id') == false){
+         $query.= "JOIN airbnblist ON airbnbhost.Host_Id = airbnblist.Host_Id";
+      }
+      $params[] = 'Rating >= :rating';
+   }
+   if (isset($_POST['airbnbPrice']) && $_POST['airbnbPrice']!='any') {
+      if(strpos($query,'JOIN airbnblist ON airbnbhost.Host_Id') == false){
+         $query.= "JOIN airbnblist ON airbnbhost.Host_Id = airbnblist.Host_Id";
+      }
+      $params[] = 'Price <= :price';
+   }
+   if (isset($_POST['airbnbBedType']) && $_POST['airbnbBedType']!='any') {
+      if(strpos($query,'JOIN airbnblist ON airbnbhost.Host_Id') == false){
+         $query.= "JOIN airbnblist ON airbnbhost.Host_Id = airbnblist.Host_Id";
+      }
+      $params[] = "Bed_type LIKE CONCAT( '%', :bedtype, '%' )";
+   }
+   if (isset($_POST['airbnbRoomType']) && $_POST['airbnbRoomType']!='any') {
+      if(strpos($query,'JOIN airbnblist ON airbnbhost.Host_Id') == false){
+         $query.= "JOIN airbnblist ON airbnbhost.Host_Id = airbnblist.Host_Id";
+      }
+      $params[] = "Room_type LIKE CONCAT( '%', :roomtype, '%' )";
+   }
+   if (isset($_POST['airbnbHostID']) && $_POST['airbnbHostID']!='any') {
+     if(strpos($query,'JOIN airbnblist ON airbnbhost.Host_Id') == true){
+         $params[] = 'airbnbhost.Host_ID=:hostid';
+      }else{
+         $params[] = 'Host_ID=:hostid';
+      }
+   }
+   if (isset($_POST['airbnbHostName']) && $_POST['airbnbHostName']!='any') {
+      if(strpos($query,'JOIN airbnblist ON airbnbhost.Host_Id') == true){
+          $params[] = "airbnbhost.First_name LIKE CONCAT( '%', :hostname, '%' ) ";
+       }else{
+          $params[] = "First_name LIKE CONCAT( '%', :hostname, '%' )";
+       }
+    }
+   if (isset($_POST['airbnbHostVerification']) && $_POST['airbnbHostVerification']!='any') {
+      if(strpos($query,'JOIN airbnblist ON airbnbhost.Host_Id') == true){
+          $params[] = 'airbnbhost.Is_verified=:verified';
+       }else{
+          $params[] = 'Is_verified=:verified';
+       }
+    }
+   if (!empty($params)) {
+      $query .= ' where ' . implode(' AND ', $params);
+   }
+   $statement = $db->prepare($query);
+   if (isset($_POST['airbnbListingName']) && $_POST['airbnbListingName']!='any') {
+      $statement->bindValue(':listing_id', $_POST['airbnbListingName']);
+   }
+   if (isset($_POST['airbnbLocation']) && $_POST['airbnbLocation']!='any') {
+      $statement->bindValue(':location', $_POST['airbnbLocation']);
+   }
+   if (isset($_POST['airbnbPrice']) && $_POST['airbnbPrice']!='any') {
+      $statement->bindValue(':price', $_POST['airbnbPrice']);
+   }
+   if (isset($_POST['airbnbBedType']) && $_POST['airbnbBedType']!='any') {
+      $statement->bindValue(':bedtype', $_POST['airbnbBedType']);
+   }
+   if (isset($_POST['airbnbRoomType']) && $_POST['airbnbRoomType']!='any') {
+      $statement->bindValue(':roomtype', $_POST['airbnbRoomType']);
+   }
+   if (isset($_POST['airbnbHostID']) && $_POST['airbnbHostID']!='any') {
+      $statement->bindValue(':hostid', $_POST['airbnbHostID']);
+   }
+   if (isset($_POST['airbnbHostName']) && $_POST['airbnbHostName']!='any') {
+      $statement->bindValue(':hostname', $_POST['airbnbHostName']);
+   }
+   if (isset($_POST['airbnbRating']) && $_POST['airbnbRating']!='any') {
+      $statement->bindValue(':rating', $_POST['airbnbRating']);
+   }
+   if (isset($_POST['airbnbAmenities']) && $_POST['airbnbAmenities']!='any') {
+      $statement->bindValue(':amenity', $_POST['airbnbAmenities']);
+   }
+   if (isset($_POST['airbnbHostVerification']) && $_POST['airbnbHostVerification']!='any') {
+      if($_POST['airbnbHostVerification']=='T'){
+         $statement->bindValue(':verified','1');
+      }elseif($_POST['airbnbHostVerification']=='F'){
+         $statement->bindValue(':verified','0');
+      }else{
+         $statement->bindValue(':verified', $_POST['airbnbHostVerification']);
+      }
+   }
+   $statement->execute();
+   $results = $statement->fetchAll();
+   $statement->closecursor();
+
+   if ($query != "select * from airbnbhost ") {
+     // Inserting into query_history table
+     $user_id = $_SESSION['loggedin'];
+     $query = "INSERT INTO `query_history` (`User_ID`, `Query_ID`, `Date_Time`) VALUES ('$user_id', NULL, current_timestamp())";
+     $statement = $db->prepare($query);
+     $statement->execute();
+     $statement->closecursor();
+
+     // Inserting into query_history_airbnb and query_history_airbnb_amenities tables
+     foreach ($results as $result) {
+       $host_id = $result['Host_ID'];
+       if(isset($result['Listing_ID'])) {
+         $listing_id = $result['Listing_ID'];
+         $query = "INSERT INTO `query_history_airbnb` (`User_ID`, `Query_ID`, `Date_Time`, `Host_ID`, `Listing_ID`)
+         VALUES ('$user_id', NULL, current_timestamp(), '$host_id', '$listing_id')";
+         $statement = $db->prepare($query);
+         $statement->execute();
+         $statement->closecursor();
+         if (isset($result['Amenity'])) {
+           $amenity = substr($result['Amenity'], 0, 255);
+           $query = "INSERT INTO `query_history_airbnb_amenities` (`User_ID`, `Query_ID`, `Listing_ID`, `Amenity`)
+           VALUES ('$user_id', NULL, '$listing_id', '$amenity')";
+           $statement = $db->prepare($query);
+           $statement->execute();
+           $statement->closecursor();
+         }
+       }
+       else {
+         $query = "INSERT INTO `query_history_airbnb` (`User_ID`, `Query_ID`, `Date_Time`, `Host_ID`, `Listing_ID`)
+         VALUES ('$user_id', NULL, current_timestamp(), '$host_id', NULL)";
+         $statement = $db->prepare($query);
+         $statement->execute();
+         $statement->closecursor();
+       }
+     }
+   }
+
+   return $results;
+}
+
+function getCrime(){
+    global $db;
+    $query = "select * from arrest ";
+    if (isset($_POST['arrestID']) && $_POST['arrestID'] != 'any') {
+        $params[] = 'ArrestID=:arrestID';
+    }
+    if (isset($_POST['arrestDate']) && $_POST['arrestDate'] != '') {
+        $params[] = 'Date=:arrestDate';
+    }
+    if (isset($_POST['arrestType']) && $_POST['arrestType'] != 'any') {
+        $params[] = "Type LIKE CONCAT('%', :arrestType, '%')";
+    }
+    if (isset($_POST['arrestGender']) && $_POST['arrestGender'] != 'any') {
+        $params[] = "Gender=:arrestGender";
+    }
+    if (isset($_POST['arrestAgeGroup']) && $_POST['arrestAgeGroup'] != 'any') {
+        $params[] = "Age_Group=:arrestAgeGroup";
+    }
+    if (isset($_POST['arrestRace']) && $_POST['arrestRace'] != 'any') {
+        $params[] = "Race=:arrestRace";
+    }
+    if (!empty($params)) {
+        $query .= ' WHERE ' . implode(' AND ', $params);
+    }
+    $statement = $db->prepare($query);
+    if (isset($_POST['arrestID']) && $_POST['arrestID'] != 'any') {
+        $statement->bindValue('arrestID', $_POST['arrestID']);
+    }
+    if (isset($_POST['arrestDate']) && $_POST['arrestDate'] != '') {
+        $statement->bindValue('arrestDate', $_POST['arrestDate']);
+    }
+    if (isset($_POST['arrestType']) && $_POST['arrestType'] != 'any') {
+        $statement->bindValue('arrestType', $_POST['arrestType']);
+    }
+    if (isset($_POST['arrestGender']) && $_POST['arrestGender'] != 'any') {
+        $statement->bindValue('arrestGender', $_POST['arrestGender']);
+    }
+    if (isset($_POST['arrestAgeGroup']) && $_POST['arrestAgeGroup'] != 'any') {
+        $statement->bindValue('arrestAgeGroup', $_POST['arrestAgeGroup']);
+    }
+    if (isset($_POST['arrestRace']) && $_POST['arrestRace'] != 'any') {
+        $statement->bindValue('arrestRace', $_POST['arrestRace']);
+    }
+    $statement->execute();
+    $results = $statement->fetchAll();
+    $statement->closecursor();
+
+    if ($query != "select * from arrest ") {
+      // Inserting into query_history table
+      $user_id = $_SESSION['loggedin'];
+      $query = "INSERT INTO `query_history` (`User_ID`, `Query_ID`, `Date_Time`) VALUES ('$user_id', NULL, current_timestamp())";
+      $statement = $db->prepare($query);
+      $statement->execute();
+
+      // Inserting into query_history_crime table
+      foreach ($results as $result) {
+        $arrest_id = $result['ArrestID'];
+        $query = "INSERT INTO `query_history_crime` (`User_ID`, `Query_ID`, `Date_Time`, `ArrestID`) VALUES ('$user_id', NULL, current_timestamp(), ' $arrest_id')";
+        $statement = $db->prepare($query);
+        $statement->execute();
+      }
+    }
+
+    return $results;
+}
+
+function getAirlineHistory() {
+  global $db;
+  $user_id = $_SESSION['loggedin'];
+  $query = "SELECT * FROM `query_history_airline` WHERE `query_history_airline`.`User_ID` = '$user_id';";
+  $statement = $db->prepare($query);
+  $statement->execute();
+  $results = $statement->fetchAll();
+  $statement->closecursor();
+  return $results;
+}
+
+function getAirbnbHistory() {
+  global $db;
+  $user_id = $_SESSION['loggedin'];
+  $query = "SELECT * FROM `query_history_airbnb` WHERE `query_history_airbnb`.`User_ID` = '$user_id';";
+  $statement = $db->prepare($query);
+  $statement->execute();
+  $results = $statement->fetchAll();
+  $statement->closecursor();
+  return $results;
+}
+
+function getCrimeHistory() {
+  global $db;
+  $user_id = $_SESSION['loggedin'];
+  $query = "SELECT * FROM `query_history_crime` WHERE `query_history_crime`.`User_ID` = '$user_id';";
+  $statement = $db->prepare($query);
+  $statement->execute();
+  $results = $statement->fetchAll();
+  $statement->closecursor();
+  return $results;
+}
+
+?>
